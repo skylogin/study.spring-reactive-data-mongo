11cf1158ceb2bc15d78ac5c4fabb2ca8a6a1653f
skylogin <skylogin@gmail.com>
2022-02-14 22:33:10 +0900

    blockhound 설치

--- src/main/java/com/greglturnquist/hackingspringboot/reactive/HackingSpringBootChapter2ReactiveApplication.java
+++ src/main/java/com/greglturnquist/hackingspringboot/reactive/HackingSpringBootChapter2ReactiveApplication.java
@@ -2,11 +2,18 @@ package com.greglturnquist.hackingspringboot.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class HackingSpringBootChapter2ReactiveApplication {
 
   public static void main(String[] args) {
+    BlockHound.builder()
+        .allowBlockingCallsInside(
+            TemplateEngine.class.getCanonicalName(), "process"
+        ).install();
+
     SpringApplication.run(HackingSpringBootChapter2ReactiveApplication.class, args);
   }
 
